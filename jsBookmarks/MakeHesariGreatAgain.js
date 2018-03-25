1a13285c666fa444f216735248ec8fbfaca6714f
Sortsimies <alatalo.miikka@gmail.com>
2018-03-25 14:42:31 +0300

    add mielipide to MHGA

--- jsBookmarks/MakeHesariGreatAgain.js
+++ jsBookmarks/MakeHesariGreatAgain.js
@@ -1,7 +1,13 @@
 var list = document.getElementsByTagName("a");
 var toBeRemoved = [];
 for(var i=0;i<list.length;i++) {
-    if (list[i].getElementsByClassName("column-icon").length > 0 || list[i].getElementsByClassName("premium-subscribers-note").length > 0 || list[i].href.includes("/nyt/")) toBeRemoved.push(list[i])
+    var mielipidelist = list[i].getElementsByClassName("teaser-category");
+    if (list[i].getElementsByClassName("column-icon").length > 0 || 
+    list[i].getElementsByClassName("premium-subscribers-note").length > 0 || 
+    list[i].href.includes("/nyt/") ||
+    (mielipidelist[0] && mielipidelist[0].innerHTML.includes("Mielipide"))){ 
+        toBeRemoved.push(list[i])
+    }
 }
 for(var j=toBeRemoved.length-1;j>-1;j--){
     toBeRemoved[j].remove();
