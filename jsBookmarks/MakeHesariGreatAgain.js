8256825cd1ff97da043fb2e808e72545f13f23c4
Sortsimies <alatalo.miikka@gmail.com>
2018-04-11 21:19:05 +0300

    add storylogo

--- jsBookmarks/MakeHesariGreatAgain.js
+++ jsBookmarks/MakeHesariGreatAgain.js
@@ -1,12 +1,14 @@
 var list = document.getElementsByTagName("a");
 var toBeRemoved = [];
 for(var i=0;i<list.length;i++) {
-    var mielipidelist = list[i].getElementsByClassName("teaser-category");
-    if (list[i].getElementsByClassName("column-icon").length > 0 || 
-    list[i].getElementsByClassName("premium-subscribers-note").length > 0 || 
+    var teaserList = list[i].getElementsByClassName("teaser-category");
+    var storyList = list[i].getElementsByClassName("storylogo");
+    if (list[i].getElementsByClassName("column-icon").length > 0 ||
+    list[i].getElementsByClassName("premium-subscribers-note").length > 0 ||
     list[i].href.includes("/nyt/") ||
-    (mielipidelist[0] && mielipidelist[0].innerHTML.includes("Mielipide"))){ 
-        toBeRemoved.push(list[i])
+    (storyList[0] && storyList[0].innerHTML.includes("Kommentti")) ||
+    (teaserList[0] && teaserList[0].innerHTML.includes("Mielipide"))) {
+      toBeRemoved.push(list[i])
     }
 }
 for(var j=toBeRemoved.length-1;j>-1;j--){
